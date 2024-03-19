6686c07245d775f8c38d07ccf62ec1e751013dcb
RVishalkumar <107658880+RVishalkumar@users.noreply.github.com>
2024-03-19 16:37:37 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def mavenBuild()
 {
   sh "mvn package"
 }
+def tomcatDeploy(jobname,ip,content)
+{
+  sh ' scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${content}.war'
+}
